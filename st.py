1f8cf9f44bc1d73e5a91aac14cfe6a46c4615f0c
Craig Phillips <craigphillips@craigphillips.net>
2018-11-09 14:31:38 -0600

    Commit from script

--- st.py
+++ st.py
@@ -23,15 +23,17 @@ current_message = ''
 #
 def send_telegram_message(message, garage_door):
 #	r1 = requests.get('https://' + telegram_url + '/getUpdates?offset=1')
-	telegram_file = open(TELEGRAM_URL_FILE, 'a')
+	telegram_file = open(TELEGRAM_URL_FILE, 'r')
 	telegram_url = telegram_file.read()
 	if garage_door == GARAGE_DOOR_1:
 		print ("GD1 write to: " + telegram_url)
-		message = 'https://' + telegram_url + '/sendMessage?chat_id=125811912&reply_markup={"keyboard": [["Close Garage Door 1"], ["Get Photo"], ["Get Video"], ["Ignore"]],"one_time_keyboard": true}&parse_mode=Markdown&text=' + message
+		message = 'https://' + telegram_url + '/sendMessage?chat_id=765138485&reply_markup={"keyboard": [["Close Garage Door 1"], ["Get Photo"], ["Get Video"], ["Ignore"]],"one_time_keyboard": true}&parse_mode=Markdown&text=' + message
+		
 	elif garage_door == GARAGE_DOOR_2:
 		print ("GD2 write to: " + telegram_url)
-		message = 'https://' + telegram_url + '/sendMessage?chat_id=125811912&reply_markup={"keyboard": [["Close Garage Door 2"], ["Get Photo"], ["Get Video"], ["Ignore"]],"one_time_keyboard": true}&parse_mode=Markdown&text=' + message
+		message = 'https://' + telegram_url + '/sendMessage?chat_id=765138485&reply_markup={"keyboard": [["Close Garage Door 2"], ["Get Photo"], ["Get Video"], ["Ignore"]],"one_time_keyboard": true}&parse_mode=Markdown&text=' + message
 	r2 = requests.post(message)
+#	print r2
 	return r2.json()
 
 def write_message_to_log(message):
@@ -62,6 +64,7 @@ while True:
 	new_message_count = len(messages) - previous_message_count
 	while new_message_count > 0:
 		current_message = messages[len(messages) - new_message_count].split()
+#		print current_message
 		if float(current_message[0]) > time.time() - MESSAGE_TIMER * 60:
 			print messages[len(messages) - new_message_count]
 			if int(current_message[1]) == GARAGE_DOOR_1:
